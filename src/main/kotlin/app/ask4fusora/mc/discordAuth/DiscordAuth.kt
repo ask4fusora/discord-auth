a63b386a952681a43d45e7de9c0c818420130285
ask4fusora <ask4fusora+github@gmail.com>
2025-11-27 17:53:22 +0700

    Initial commit

---
+++ src/main/kotlin/app/ask4fusora/mc/discordAuth/DiscordAuth.kt
@@ -0,0 +1,14 @@
+package app.ask4fusora.mc.discordAuth
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DiscordAuth : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
